6e48e943e023ccfbc76aa6bad71b7b521f5a5af2
mockono <gisellymae@gmail.com>
2026-09-09 11:38:25 +0100

    Update competitionsConfig.ts

--- lib/data/competitionsConfig.ts
+++ lib/data/competitionsConfig.ts
@@ -15,4 +15,9 @@ export interface LeagueConfig {
 
 export const ACTIVE_LEAGUES: LeagueConfig[] = [
   { code: "PL", name: "Premier League", country: "Angleterre", oddsSportKey: "soccer_epl" },
+  { code: "PD", name: "La Liga", country: "Espagne", oddsSportKey: "soccer_spain_la_liga" },
+  { code: "SA", name: "Serie A", country: "Italie", oddsSportKey: "soccer_italy_serie_a" },
+  { code: "FL1", name: "Ligue 1", country: "France", oddsSportKey: "soccer_france_ligue_one" },
+  { code: "BL1", name: "Bundesliga", country: "Allemagne", oddsSportKey: "soccer_germany_bundesliga" },
+  { code: "CL", name: "UEFA Champions League", country: "Europe", oddsSportKey: "soccer_uefa_champs_league" },
 ];
